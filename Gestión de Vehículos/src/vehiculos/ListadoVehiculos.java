fcab8b8cfe64c67ce9b45f470b22d3e8d0af4e94
RodriguezDAW1 <josea.rodlop.1@educa.jcyl.es>
2022-04-12 21:29:50 +0200

    Se crea la clase del listado de vehículos.

---
+++ Gestión de Vehículos/src/vehiculos/ListadoVehiculos.java
@@ -0,0 +1,19 @@
+package vehiculos;
+
+import java.util.ArrayList;
+
+/**
+ * Clase que representa a todo el conjunto de vehículos.
+ * @author José A. Rodríguez López.
+ * @version 1.0
+ */
+public class ListadoVehiculos {
+	private ArrayList<Vehiculos> listadoVehiculos=null;	//Conjunto de vehículos.
+	
+	/**
+	 * Cóntructor ListadoVehiculos.
+	 */
+	public ListadoVehiculos() {
+		listadoVehiculos=new ArrayList<>();
+	}
+}
